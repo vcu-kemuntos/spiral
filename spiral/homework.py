3050b244f9999cf5a0b3eec863c4d2efbd9bb4bf
Stacy Kemunto <kemuntos@vcu.edu>
2021-04-05 20:21:10 -0400

    4

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,5 +1,5 @@
 def spiralize(number):
-    n=(number-1)//2
-    return_value = (16*n*n*n + 30*n*n + 26*n +3)//3
+    n = (number - 1) // 2
+    return_value = (16 * n * n * n + 30 * n * n + 26 * n + 3) // 3
 
     return return_value
